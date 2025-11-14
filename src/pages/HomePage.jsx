62a6a2546793bb7d543715ea009d2532d9fcd388
NontasBak <nontasbak@gmail.com>
2025-11-14 12:37:44 +0200

    Display park button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,19 @@
 import Map from "../components/Map";
+import ParkButton from "../components/ParkButton";
 import { useParkingContext } from "../context/ParkingContext";
 
 export default function HomePage() {
   const { currentLocation, cameraLocation, marker, parkingLocations } = useParkingContext();
 
   return (
-    <Map
-      currentLocation={currentLocation}
-      cameraLocation={cameraLocation}
-      marker={marker}
-      parkingLocations={parkingLocations}
-    />
+    <div className="relative w-full h-screen">
+      <Map
+        currentLocation={currentLocation}
+        cameraLocation={cameraLocation}
+        marker={marker}
+        parkingLocations={parkingLocations}
+      />
+      <ParkButton />
+    </div>
   );
 }
